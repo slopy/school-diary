4af23e7f5e156cfcbcd53239062c0d77cbafb66c
Slopy <mic.mar@live.com>
2016-08-12 17:06:14 +0200

    Bring back devise to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable,
+   :registerable, :recoverable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :user
+
   resources :students do
     get :subjects
   end
