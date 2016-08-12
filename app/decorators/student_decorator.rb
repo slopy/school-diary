0138f2f918e3c3fc8b856f2bb21385bdee872148
Slopy <mic.mar@live.com>
2016-08-12 17:53:55 +0200

    Fix Student decorator #avg_notes

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,16 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    values = subject_item.subject_item_notes
+                         .sum(:value)
+
+    divider = subject_item.subject_item_notes
+                          .count
+
+    if values && divider > 0
+      format("%.2f",values.to_f/divider.to_f)
+    else
+      format("%.2f",0)
+    end
   end
 end
