0cd1ae5ad970d2ef365a9914b25624b0c502e003
Slopy <mic.mar@live.com>
2016-08-14 13:36:17 +0200

    Remove SubjectItem not needed Student reference
    SubjectItem has_many students throuth Participation
    it does not belongs_to student

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812142949) do
+ActiveRecord::Schema.define(version: 20160814113011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +49,8 @@ ActiveRecord::Schema.define(version: 20160812142949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "teacher_id"
-    t.integer  "student_id"
   end
 
-  add_index "subject_items", ["student_id"], name: "index_subject_items_on_student_id", using: :btree
   add_index "subject_items", ["teacher_id"], name: "index_subject_items_on_teacher_id", using: :btree
 
   create_table "teachers", force: :cascade do |t|
@@ -85,6 +83,5 @@ ActiveRecord::Schema.define(version: 20160812142949) do
   add_foreign_key "participations", "subject_items"
   add_foreign_key "subject_item_notes", "students"
   add_foreign_key "subject_item_notes", "subject_items"
-  add_foreign_key "subject_items", "students"
   add_foreign_key "subject_items", "teachers"
 end
