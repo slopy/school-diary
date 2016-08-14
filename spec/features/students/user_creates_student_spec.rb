a9c2c4785d4f1c2124c939cffa0a93824fcceec9
Slopy <mic.mar@live.com>
2016-08-14 19:28:27 +0200

    Add specs for setting birthdate

--- spec/features/students/user_creates_student_spec.rb
+++ spec/features/students/user_creates_student_spec.rb
@@ -27,3 +27,23 @@ feature 'User creates student' do
     expect(page).to have_content "can't be blank"
   end
 end
+
+feature 'User creates student student birthdate' do
+
+  scenario 'with birthdate datepicker', js: true do
+    user = build(:user)
+    sign_up_with user.email, user.password
+
+    expect(page).to have_content('Logout')
+
+    visit students_path
+    click_on 'Add new one'
+
+    fill_in 'First name', with: 'Luk'
+    fill_in 'Last name', with: 'Skywalker'
+    page.execute_script("$('.form_datetime').datepicker('update', '1992-02_01');")
+    click_button 'Create Student'
+    expect(page).to have_content '1992-02_01'
+  end
+
+end
