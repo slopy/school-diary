544d24a4d1f2caf2993a9cdde4c146da32311432
Slopy <mic.mar@live.com>
2016-08-14 14:28:41 +0200

    Add random birthdate column to user factory
    This will set random date or nill to birthdate column

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :student do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    birthdate do 
+    	date_from = 19.years.ago
+    	date_to = 25.years.ago
+    	date = Faker::Date.between(date_from, date_to)
+    	[true, false].sample ? nil : date
+    end
   end
 end
