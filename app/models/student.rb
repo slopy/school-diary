da2c51d67322774904d8cdedb0b1cce4892b0f71
Slopy <mic.mar@live.com>
2016-08-15 02:22:50 +0200

    Add missing schedule_at attribute to students

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ActiveRecord::Base
   has_many :participations, dependent: :destroy
   has_many :subject_item_notes, dependent: :destroy
   has_many :subject_items, through: :participations
-  has_many :payments
+  has_many :payments, -> { joins(:payment_date).order('payment_dates.scheduled_at') }
   has_many :payments_dates, through: :payments
 
   validates :first_name, :last_name, presence: true
