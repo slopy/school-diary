33ff496f358962152669a32b71d95de264ec8a50
Slopy <mic.mar@live.com>
2016-08-15 02:23:08 +0200

    Order payment in payment table

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,4 +4,6 @@ class Payment < ActiveRecord::Base
 
   validates :student, :payment_date, presence: true
   validates :payment_date, uniqueness: { scope: :student }
+
+  delegate :scheduled_at, to: :payment_date
 end
